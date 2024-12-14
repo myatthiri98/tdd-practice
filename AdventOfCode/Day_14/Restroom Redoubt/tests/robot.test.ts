47141c90f836bda5ed2528952f14629cb14d163e
Myatthiri <myatthirikhin1998@gmail.com>
2024-12-14 22:39:33 +0630

    refactor: test file

--- AdventOfCode/Day_14/Restroom Redoubt/tests/robot.test.ts
+++ AdventOfCode/Day_14/Restroom Redoubt/tests/robot.test.ts
@@ -7,24 +7,24 @@ import {
 } from '../src/robot'
 
 describe('Robot movement', () => {
-  it('calculates the new position correctly after 1 second', () => {
-    const position = { x: 2, y: 4 }
-    const velocity = { x: 3, y: -3 }
-    const grid = { width: 11, height: 7 }
-
+  it.each([
+    {
+      position: { x: 2, y: 4 },
+      velocity: { x: 3, y: -3 },
+      grid: { width: 11, height: 7 },
+      expected: { x: 5, y: 1 },
+      description: 'calculates the new position correctly after 1 second',
+    },
+    {
+      position: { x: 10, y: 6 },
+      velocity: { x: 3, y: 2 },
+      grid: { width: 11, height: 7 },
+      expected: { x: 2, y: 1 },
+      description: 'wraps around when moving out of bounds',
+    },
+  ])('$description', ({ position, velocity, grid, expected }) => {
     const newPosition = getNewPosition(position, velocity, grid)
-
-    expect(newPosition).toEqual({ x: 5, y: 1 })
-  })
-
-  it('wraps around when moving out of bounds', () => {
-    const position = { x: 10, y: 6 }
-    const velocity = { x: 3, y: 2 }
-    const grid = { width: 11, height: 7 }
-
-    const newPosition = getNewPosition(position, velocity, grid)
-
-    expect(newPosition).toEqual({ x: 2, y: 1 }) // Wrapped around
+    expect(newPosition).toEqual(expected)
   })
 
   it('simulates movement over multiple seconds', () => {
@@ -43,20 +43,25 @@ describe('Robot movement', () => {
     ])
   })
 
-  it('counts robots in each quadrant after 100 seconds', () => {
-    const robots: Robot[] = [
-      { position: { x: 6, y: 2 }, velocity: { x: 1, y: -1 } }, // Will end up in Q1
-      { position: { x: 2, y: 2 }, velocity: { x: -1, y: -1 } }, // Will end up in Q2
-      { position: { x: 2, y: 6 }, velocity: { x: -1, y: 1 } }, // Will end up in Q3
-      { position: { x: 5, y: 5 }, velocity: { x: 0, y: 0 } }, // On center line, not counted
-    ]
-
-    const grid = { width: 10, height: 10 }
-    const counts = countRobotsInQuadrants(robots, grid, 100)
+  it.each([
+    {
+      robots: [
+        { position: { x: 6, y: 2 }, velocity: { x: 1, y: -1 } }, // Will end up in Q1
+        { position: { x: 2, y: 2 }, velocity: { x: -1, y: -1 } }, // Will end up in Q2
+        { position: { x: 2, y: 6 }, velocity: { x: -1, y: 1 } }, // Will end up in Q3
+        { position: { x: 5, y: 5 }, velocity: { x: 0, y: 0 } }, // On center line, not counted
+      ],
+      grid: { width: 10, height: 10 },
+      seconds: 100,
+      expected: { Q1: 1, Q2: 1, Q3: 1, Q4: 0 },
+      description: 'counts robots in each quadrant after 100 seconds',
+    },
+  ])('$description', ({ robots, grid, seconds, expected }) => {
+    const counts = countRobotsInQuadrants(robots, grid, seconds)
 
     console.log('Final robot positions:', robots)
     console.log('Counts by quadrant:', counts)
 
-    expect(counts).toEqual({ Q1: 1, Q2: 1, Q3: 1, Q4: 0 })
+    expect(counts).toEqual(expected)
   })
 })
