aef7d029ef8428d4abc7f2c9d20eac4893465744
myatthiri98 <myatthirikhin1998@gmail.com>
2025-01-07 10:36:39 +0630

    chore: refactor test with edge cases

--- Practice/Day_14/Problem_14/tests/containerWithMostWater.test.ts
+++ Practice/Day_14/Problem_14/tests/containerWithMostWater.test.ts
@@ -4,36 +4,41 @@ import { maxArea } from '../src/containerWithMostWater'
 describe('maxArea', () => {
   it.each([
     {
-      numbers: [1],
+      heights: [1],
       expected: 0,
       description: 'return 0 for an array with less than two heights',
     },
     {
-      numbers: [1, 8, 6, 2, 5, 4, 8, 3, 7],
+      heights: [1, 8, 6, 2, 5, 4, 8, 3, 7],
       expected: 49,
       description: 'calculate the correct maximum area',
     },
     {
-      numbers: [1, 1],
+      heights: [1, 1],
       expected: 1,
       description: 'handle simple case with two equal heights',
     },
     {
-      numbers: [4, 3, 2, 1, 4],
+      heights: [4, 3, 2, 1, 4],
       expected: 16,
       description: 'calculate correct area with multiple heights',
     },
     {
-      numbers: [1, 2, 1],
+      heights: [1, 2, 1],
       expected: 2,
       description: 'handle cases with varying heights',
     },
     {
-      numbers: [5, 5, 5, 5],
+      heights: [5, 5, 5, 5],
       expected: 15,
       description: 'handle arrays with all elements the same',
     },
-  ])('should $description: numbers = $numbers', ({ numbers, expected }) => {
-    expect(maxArea(numbers)).toBe(expected)
+    {
+      heights: [1, 1000, 1, 1000],
+      expected: 2000,
+      description: 'extreme values on both ends',
+    },
+  ])('should $description: heights = $heights', ({ heights, expected }) => {
+    expect(maxArea(heights)).toBe(expected)
   })
 })
