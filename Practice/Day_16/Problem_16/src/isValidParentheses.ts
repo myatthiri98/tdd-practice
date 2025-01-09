5df9099eba34953297e1cd6b82f80340a498a1dd
myatthiri98 <myatthirikhin1998@gmail.com>
2025-01-09 14:28:16 +0630

    chore: refactor codes for Valid Parentheses

--- Practice/Day_16/Problem_16/src/isValidParentheses.ts
+++ Practice/Day_16/Problem_16/src/isValidParentheses.ts
@@ -1,22 +1,19 @@
 export function isValidParentheses(s: string): boolean {
   const stack: string[] = []
-
-  const map: { [key: string]: string } = {
+  const map: Record<string, string> = {
     '(': ')',
     '{': '}',
     '[': ']',
   }
 
-  for (let i = 0; i < s.length; i++) {
-    const char = s[i]
-
+  for (let char of s) {
     if (map[char]) {
       // If it's an opening bracket, push it to the stack
       stack.push(char)
     } else {
       // If it's a closing bracket, check if the last opened one matches
       const last = stack.pop()
-      if (map[last!] !== char) {
+      if (!last || map[last] !== char) {
         return false
       }
     }
--- Practice/Day_16/Problem_16/tests/validParentheses.test.ts
+++ Practice/Day_16/Problem_16/tests/validParentheses.test.ts
@@ -1,37 +1,18 @@
 import { describe, expect, it } from 'vitest'
 import { isValidParentheses } from '../src/isValidParentheses'
 
-describe('Valid Parentheses', () => {
+describe('isValidParentheses', () => {
   it.each([
-    {
-      input: '()',
-      expected: true,
-    },
-    {
-      input: '()[]{}',
-      expected: true,
-    },
-    {
-      input: '(]',
-      expected: false,
-    },
-    {
-      input: '([)]',
-      expected: false,
-    },
-    {
-      input: '{[]}',
-      expected: true,
-    },
-    {
-      input: 'abc',
-      expected: false,
-    },
-    {
-      input: '',
-      expected: true,
-    },
-  ])('should return %p for input "%s"', ({ input, expected }) => {
+    ['[]', true],
+    ['()', true],
+    ['()[]{}', true],
+    ['([{}])', true],
+    ['(]', false],
+    ['([)]', false],
+    ['{[]}', true],
+    ['abc', false],
+    ['', true],
+  ])('should return %p for input "%s"', (input, expected) => {
     expect(isValidParentheses(input)).toBe(expected)
   })
 })
