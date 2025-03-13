747ef4c4739656d2c8c557fd2008209546debf53
Myatthiri <myatthirikhin1998@gmail.com>
2025-03-13 19:36:25 +0630

    feat: solve tree sum

--- Practice/Day_74/src/treeSum.ts
+++ Practice/Day_74/src/treeSum.ts
@@ -18,3 +18,19 @@ export function treeSum(root: Node | null): number {
   if (!root) return 0
   return root.value + treeSum(root.left) + treeSum(root.right)
 }
+
+// export function treeSum(root: Node | null): number {
+//     if (!root) return 0;
+
+//     let sum = 0;
+//     const queue: Node[] = [root];
+
+//     while (queue.length) {
+//       const node = queue.shift()!;
+//       sum += node.value;
+//       if (node.left) queue.push(node.left);
+//       if (node.right) queue.push(node.right);
+//     }
+
+//     return sum;
+//   }
