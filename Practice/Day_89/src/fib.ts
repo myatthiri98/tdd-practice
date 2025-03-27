a78cea41f08a5a39bfba8eb80a276d1524975230
Myatthiri <myatthirikhin1998@gmail.com>
2025-03-27 20:44:40 +0630

    feat: solve fib

--- Practice/Day_89/src/fib.ts
+++ Practice/Day_89/src/fib.ts
@@ -12,3 +12,14 @@ export function fib(n: number): number {
 
   return curr
 }
+
+// function fib(n, memo = {}) {
+//     if (n in memo) return memo[n];  // If already computed, return from cache
+//     if (n === 0) return 0;          // Base case 1
+//     if (n === 1) return 1;          // Base case 2
+
+//     memo[n] = fib(n - 1, memo) + fib(n - 2, memo);  // Store the computed value
+//     return memo[n];
+// }
+
+// console.log(fib(50)); // Output: 12586269025 (Runs much faster!)
