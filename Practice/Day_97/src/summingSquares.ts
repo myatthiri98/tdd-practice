1b1a5df71be8defcf4fd12b0eb9994779ab11a8e
Myatthiri <myatthirikhin1998@gmail.com>
2025-04-01 23:24:36 +0630

    chore: add problem description

--- Practice/Day_97/src/summingSquares.ts
+++ Practice/Day_97/src/summingSquares.ts
@@ -1,3 +1,8 @@
+// summing squares
+// Write a function, summingSquares, that takes a target number as an argument. The function should return the minimum number of perfect squares that sum to the target. A perfect square is a number of the form (i*i) where i >= 1.
+
+// For example: 1, 4, 9, 16 are perfect squares, but 8 is not perfect square.
+
 export const summingSquares = (
   n: number,
   memo: Record<number, number> = {},
